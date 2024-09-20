bdd548a5019a59cf8d09a3858ec0e280afb080d0
Charles Lowell <cowboyd@frontside.com>
2024-09-20 17:36:44 -0500

    ✨ Add sitemap.xml
    Having a sitemap to all of our sites will allow us to statically
    generate across all the different content repositories that we have.
    
    This is a simple site, with simple content, so all that is needed is
    to add `sitemap.xml.ts` route that walks across the document table of
    contents stored in `toc.json` and create the manifest of urls
    contained therein.

---
+++ www/routes/sitemap.xml.ts
@@ -0,0 +1,40 @@
+import { Handlers } from "$fresh/server.ts";
+import { stringify } from "jsr:@libs/xml";
+
+import docs from "../docs/toc.json" with { type: "json" };
+
+export const handler: Handlers = {
+  GET(request) {
+    let base = new URL(
+      request.headers.get("x-base-url") ?? new URL(new URL(request.url).origin),
+    );
+    let paths = Object.entries(docs).flatMap(([topic, collection]) => {
+      if ("pages" in collection) {
+        return collection.pages.map(([id]) => `${topic}/${id}`);
+      } else {
+        return topic;
+      }
+    });
+
+    let index = { loc: String(base) };
+
+    let urls = paths.map((path) => ({
+      loc: String(new URL(path, base)),
+    }));
+
+    let xml = stringify({
+      "@version": "1.0",
+      "@encoding": "UTF-8",
+      urlset: {
+        "@xmlns": "http://www.sitemaps.org/schemas/sitemap/0.9",
+        url: [index, ...urls],
+      },
+    });
+
+    return new Response(xml, {
+      headers: {
+        "Content-Type": "application/xml",
+      },
+    });
+  },
+};
